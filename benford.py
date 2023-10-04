72985f0426f642c72ec3131369ec75a2ea1a5730
0LukasV1 <113472942+0LukasV1@users.noreply.github.com>
2023-10-04 09:31:20 +0200

    Add files via upload

---
+++ benford.py
@@ -0,0 +1,24 @@
+import random
+from Collections import Counter
+output=[]
+zoz = []
+for i in range(1,10000):
+    a =random.randrange(1,10000)
+    b =random.randrange(1,10000)
+    c = a**b
+    output.append(str(c)[0])
+
+
+with(open) as cvs_file:
+    cvs_reader
+    zoz.append(i)
+zoz.sort()
+zoz
+for i in range(1,10):
+    print(str(i)+"="+str(zoz[::-1][i]/10))
+
+
+
+
+
+
